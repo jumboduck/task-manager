35019e70b1a9fd123010ddadcf89cdf46622a835
Simon Castagna <simoncastagna@Simons-MacBook-Pro.local>
2020-06-10 15:42:57 +0200

    removed env from imports

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-import env as config
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = "task_manager"
